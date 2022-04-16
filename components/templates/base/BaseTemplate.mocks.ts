cf29e3e25a55f2ade473a07030b31d1b4cc16efd
alHasandev <alhasan99dev@gmail.com>
2022-04-16 11:22:48 +0800

    build: create BaseTemplate component

---
+++ components/templates/base/BaseTemplate.mocks.ts
@@ -0,0 +1,9 @@
+import { BaseTemplateProps } from './BaseTemplate';
+
+const base: BaseTemplateProps = {
+  sampleTextProp: 'Hello Next.js',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+};
