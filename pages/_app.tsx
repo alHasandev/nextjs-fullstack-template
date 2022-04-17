fc5c09f4eabd8fa80ba2ee4d6f683d0b129455c1
alHasandev <alhasan99dev@gmail.com>
2022-04-17 14:09:04 +0800

    build: add NextPage with layout option

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css';
+import '@styles/globals.css';
 import type { AppProps } from 'next/app';
+import { NextPageWithLayout } from './page';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || ((page) => page);
+
+  console.assert(Component.getLayout, 'Get Layout Not Exist');
+
+  return getLayout(<Component {...pageProps} />);
 }
 
 export default MyApp;
